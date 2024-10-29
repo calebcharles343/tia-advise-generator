0a8faafeb13f7b6c45734f8442338b17e7afcbc5
Charles Caleb <charlesofficial9@gmail.com>
2024-10-29 11:51:28 +0100

    update

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,106 @@
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap');
+
+/* 
+01 Typography System
+
+
+Font sizes (px)
+FONT SIZE SYSTEM (px)
+10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 / 52 / 62 / 74 / 86 / 98 /
+
+- Font weight:
+Default: 400, 
+Medium:500, 
+Semi-bold:600, 
+Bold: 700,
+Large: 900.
+
+- Line height:
+Default: 1
+Small: 1.05
+medium: 1.2
+large: 1.8
+Paragraph default: 1.6
+
+ letter-spacing
+ -0.5px
+ - 0.75px;
+
+02 COLORS
+-Primary: #background: #202733, #53ffaa 
+
+-Secondary: #BBC0FF, #FFFFFF;
+,
+-shades: 
+#45260a
+
+-accents:
+-greys: #555; #333;
+
+03 SHADOWS
+box-shadow:
+ 0 2.4rem 4.8rem rgba(0, 0, 0, 0.075);
+
+04 BORDER RADIOUS 
+default: 9px
+medium: 12px
+
+05 WHITESPACE
+
+SPACING SYSTEM (px)
+2 / 4 / 8 / 12 / 16 / 24 / 32 / 48 / 64 / 80 / 96 / 128
+
+
+
+*/
+
+/* /////////////////////// */
+/* GLOBAL RESET */
+/* /////////////////////// */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  /* font-size: 10px; */
+  /* 10px / 16px = 0.625*100 = 62.5
+  percentage of user's browser font-size settings */
+  font-size: 62.5%;
+  overflow-x: hidden;
+
+  /*does not work on safari and micro edge  */
+  /* scroll-behavior: smooth; */
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Manrope", sans-serif;
+  line-height: 1;
+  font-weight: 400;
+  color: #fff;
+  background-color: #202733;
+
+  /* Only works if ther is nothing 
+  absolutely positioned in relations to body  */
+  overflow-x: hidden;
+  overflow-y: scroll;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+ul,
+ol,
+legend {
+  list-style: none;
+}
+
+`;
+export default GlobalStyle;
---
+++ src/ui/Advise.tsx
@@ -0,0 +1,138 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const StyledAdvice = styled.div`
+  width: 54rem;
+  height: 36.4rem;
+`;
+
+const AdviceTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  background-color: #313a48;
+  height: 33.2rem;
+  width: 100%;
+  padding: 4rem;
+  border-radius: 15px;
+`;
+
+const AdviseHeader = styled.h1`
+  color: #53ffaa;
+  font-weight: 800;
+  font-size: 1.3rem;
+  line-height: 1.776rem;
+  text-align: center;
+  letter-spacing: 0.409rem;
+  margin-bottom: 2rem;
+`;
+
+const Quote = styled.p`
+  color: #cee3e9;
+  font-weight: 800;
+  font-size: 2.8rem;
+  line-height: 3.825rem;
+  text-align: center;
+  margin-bottom: 3rem;
+`;
+
+const AdviseButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  background-color: transparent;
+  border-radius: 50px;
+  width: 6.4rem;
+  height: 6.4rem;
+  border: none;
+  left: 50%;
+  bottom: 0;
+  top: 88%;
+  transform: translateX(-50%);
+  cursor: pointer;
+`;
+
+interface DataType {
+  id: number;
+  name: string;
+  // add more fields according to the API response
+}
+
+function Advise() {
+  const [data, setData] = useState<DataType[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null); // Reset error state
+
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice"); // replace with your API URL
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+
+      setData(data);
+    } catch (err) {
+      setError((err as Error).message); // Handle error
+    } finally {
+      setLoading(false); // Always set loading to false after completion
+    }
+  };
+
+  // if (loading) return <p>Loading...</p>;
+  // if (error) return <p>Error: {error}</p>;
+  return (
+    <StyledAdvice>
+      <AdviceTextContainer>
+        <AdviseHeader>ADVICE # 117</AdviseHeader>
+        <Quote>
+          “It is easy to sit up and take notice, what's difficult is getting up
+          and taking action.”
+        </Quote>
+
+        <svg
+          width="444"
+          height="16"
+          viewBox="0 0 444 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect y="8" width="196" height="1" fill="#4F5D74" />
+          <rect x="248" y="8" width="196" height="1" fill="#4F5D74" />
+          <rect x="212" width="6" height="16" rx="3" fill="#CEE3E9" />
+          <rect x="226" width="6" height="16" rx="3" fill="#CEE3E9" />
+        </svg>
+
+        <AdviseButton onClick={() => fetchData()}>
+          <svg
+            width="64"
+            height="64"
+            viewBox="0 0 64 64"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M64 32C64 49.6731 49.6731 64 32 64C14.3269 64 0 49.6731 0 32C0 14.3269 14.3269 0 32 0C49.6731 0 64 14.3269 64 32Z"
+              fill="#53FFAA"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M24 20H40C42.2081 20.0025 43.9975 21.7919 44 24V40C43.9975 42.2081 42.2081 43.9975 40 44H24C21.7919 43.9975 20.0025 42.2081 20 40V24C20.0025 21.7919 21.7919 20.0025 24 20ZM26 36.5C26 37.3284 26.6716 38 27.5 38C28.3284 38 29 37.3284 29 36.5C29 35.6716 28.3284 35 27.5 35C26.6716 35 26 35.6716 26 36.5ZM27.5 29C26.6716 29 26 28.3284 26 27.5C26 26.6716 26.6716 26 27.5 26C28.3284 26 29 26.6716 29 27.5C29 28.3284 28.3284 29 27.5 29ZM30.5 32C30.5 32.8284 31.1716 33.5 32 33.5C32.8284 33.5 33.5 32.8284 33.5 32C33.5 31.1716 32.8284 30.5 32 30.5C31.1716 30.5 30.5 31.1716 30.5 32ZM36.5 38C35.6716 38 35 37.3284 35 36.5C35 35.6716 35.6716 35 36.5 35C37.3284 35 38 35.6716 38 36.5C38 37.3284 37.3284 38 36.5 38ZM35 27.5C35 28.3284 35.6716 29 36.5 29C37.3284 29 38 28.3284 38 27.5C38 26.6716 37.3284 26 36.5 26C35.6716 26 35 26.6716 35 27.5Z"
+              fill="#202733"
+            />
+          </svg>
+        </AdviseButton>
+      </AdviceTextContainer>
+    </StyledAdvice>
+  );
+}
+
+export default Advise;
